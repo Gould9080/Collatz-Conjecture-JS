938f34e1cb304660a457e2f843db464e62950158
Rum Ham <114189738+Gould9080@users.noreply.github.com>
2022-10-04 16:58:05 -0400

    Add files via upload

---
+++ Collatz.js
@@ -0,0 +1,16 @@
+// *** THE COLLATZ CONJECTURE***
+let guess = parseInt(prompt("Enter Number"));
+let attempts = 1
+while (guess > 1) {
+    if (guess % 2 === 0) {
+        guess = (guess / 2);
+        console.log(guess);
+        attempts++
+    }
+    else {
+        guess = (guess * 3 + 1)
+        console.log(guess);
+        attempts++
+    }
+}
+console.log(`It took ${attempts} attempts to reach 1`);
